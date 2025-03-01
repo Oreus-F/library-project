3eec66708eb766c50b2bb83cd575e308ebf46549
Oreus-F <fourniol.lucas@gmail.com>
2025-03-01 18:50:54 +0100

    Create 3 books inside myLibrary

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 myLibrary = [];
 
-const Book = function(title, author, pages, edition){
+const Book = function(title, author, pages){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.edition = edition;
 }
 
 function addBookToLibrary() {
-    // take parameters, Create new book, add it to library
-}
+    // take parameters
+    let title = prompt("Title ?");
+    let author = prompt("Author ?");
+    let pages = prompt("Pages ?");
+
+    //Create new book
+    const book = new Book(title, author, pages);
+    
+    //add it to library
+    myLibrary.push(book);
+};
+
+const book1 = new Book("1984","George Orwell", 391);
+const book2 = new Book("The Lord of the Rings","J.R.R Tolkien", 1335);
+const book3 = new Book("The Man in the High Castle","Philip K. Dick", 396);
+
+myLibrary.push(book1, book2, book3);
+
+/* addBookToLibrary(); */
+console.log(myLibrary);
